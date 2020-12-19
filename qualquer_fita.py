30b536011d886372c46620062cd03281a6f44166
Ronan Pereira <64877410+technovikng@users.noreply.github.com>
2020-12-19 03:38:15 -0300

    teste
    teste

---
+++ qualquer_fita.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+asdfasdfasdf
+asdfasdfasdf
+adsfasdfasdf
+
+This is a temporary script file.
+"""
+# comentario
+
+
+A = 2      # asdsa
+print (A)  # asdasd
+print (f' valor da var eh {A}')
